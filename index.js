858754718c73c9bd0a62053389f5030027ce1a04
expectNothin9 <expect.nothin9@gmail.com>
2018-04-27 18:22:27 +0800

    log event and event source
    try profile
    
    remove dev log

--- index.js
+++ index.js
@@ -19,14 +19,25 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const lineBot = new line.Client(config)
 function handleEvent (event) {
+  // console.log('event', event)
+  // event {
+  //   type: 'message',
+  //   replyToken: 'xxx',
+  //   source: { userId: 'xxx', type: 'user' },
+  //   timestamp: 1524824621289,
+  //   message: { type: 'text', id: '7863626468882', text: '我是誰' }
+  // }
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null)
   }
 
-  return lineBot.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  })
+  return lineBot.getProfile(event.source.userId)
+    .then((profile) => profile.displayName)
+    .then((userName) => lineBot.replyMessage(event.replyToken, {
+      type: 'text',
+      text: `${userName}: ${event.message.text}`
+    }))
+    .catch((exception) => { console.log(exception) })
 }
 
 // heroku dev debug

a6ffa977a544df96a79355e530d09a5a1afbfa9f
expectNothin9 <expect.nothin9@gmail.com>
2018-04-28 18:35:12 +0800

    heroku addons:create mongolab AND use process.env.MONGODB_URI as connection URI

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // DB
-mongoose.connect('mongodb://localhost/HelloMongoose')
+mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
 db.on('error', (err) => { console.log('db', err) })
 db.once('open', () => { console.log('db opened') })

1a13d475638d231fd7b5e09cd56762dad1848ab3
expectNothin9 <expect.nothin9@gmail.com>
2018-04-30 16:09:14 +0800

    try template message with demo example

--- lineBot.js
+++ lineBot.js
@@ -127,7 +127,42 @@ const botUtil = {
 
   showShoppingList: ({ bot, event }) => {
     const altText = 'Shopping List'
-    return bot.replyMessage(event.replyToken, makeCarouselTemplateMessage({ altText }))
+    // return bot.replyMessage(event.replyToken, makeCarouselTemplateMessage({ altText }))
+    return bot.replyMessage(event.replyToken, {
+  "type": "template",
+  "altText": "This is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover",
+      "imageBackgroundColor": "#FFFFFF",
+      "title": "Menu",
+      "text": "Please select",
+      "defaultAction": {
+          "type": "uri",
+          "label": "View detail",
+          "uri": "http://example.com/page/123"
+      },
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+})
       .catch(log.handleException('botUtil.showShoppingList'))
   }
 }

df3a1a9d6ed8a6d128299d0c931f2f0eb3d0c58a
expectNothin9 <expect.nothin9@gmail.com>
2020-12-04 13:57:20 +0800

    debug logs of whistling

--- src/lib/whistling.ts
+++ src/lib/whistling.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-// const debug = require('debug')('R:whistling')
+const debug = require('debug')('R:whistling')
 
 let IMAGES = ['https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/e35/129060059_215457286655216_6062865869436442074_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=Iat2_GsVGy8AX8OruLy&tp=1&oh=622990499e73a3b39382d7aebc853f21&oe=5FF4BD93']
 
@@ -20,6 +20,7 @@ async function prepareImages () {
 prepareImages()
 
 export const whistlingHandler = async (req, res) => {
+  debug(IMAGES)
   const randIdx = Math.floor(Math.random() * IMAGES.length)
   const imageUrl = IMAGES[randIdx]
   const fetchResp = await fetch(imageUrl)

b60994f97e10cc6935a1d3d830e4176aefe8c028
expectNothin9 <expect.nothin9@gmail.com>
2020-12-07 16:46:56 +0800

    add dev logs of scraper

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -9,12 +9,15 @@ export const scrapIgHandler = async (req, res) => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox']
   })
-
+  debug('AFTER await puppeteer.launch')
   const page = await browser.newPage()
+  debug('AFTER await browser.newPage')
   await page.setViewport({ width: 1920, height: 1080 })
+  debug('AFTER await page.setViewport')
   await page.goto(IG_URL, {
     waitUntil: ['load', 'networkidle0', 'domcontentloaded']
   })
+  debug('AFTER await page.goto')
   const IMAGES = await page.evaluate(() => {
     const images = []
     const imgElements = document.querySelectorAll('article div > img')
@@ -23,6 +26,7 @@ export const scrapIgHandler = async (req, res) => {
     })
     return images
   })
+  debug('AFTER await page.evaluate')
   // await page.screenshot({path: path.join(__dirname, '../..', 'example.png')})
   res.send(JSON.stringify(IMAGES, null, 2))
 }
